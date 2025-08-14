fee9a5819f29e027ec01e183507362ea39023b05
Hung Pham <truonghungit@gmail.com>
2025-08-14 10:17:48 +0700

    feat: :sparkles: implement UI for sign in page

--- src/features/(authentication)/sign-in/index.tsx
+++ src/features/(authentication)/sign-in/index.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { GalleryVerticalEnd } from 'lucide-react';
+import { SignInForm } from '@/components/sign-in-form';
 
 export const Route = createFileRoute('/(authentication)/sign-in/')({
-  component: RouteComponent,
+  component: SignInPage,
 });
 
-function RouteComponent() {
-  return <div>Hello "/(authentication)/sign-in/"!</div>;
+function SignInPage() {
+  return (
+    <div className='bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10'>
+      <div className='flex w-full max-w-sm flex-col gap-6'>
+        <a
+          href='#'
+          className='flex items-center gap-2 self-center font-medium'
+        >
+          <div className='bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md'>
+            <GalleryVerticalEnd className='size-4' />
+          </div>
+          Acme Inc.
+        </a>
+        <SignInForm />
+      </div>
+    </div>
+  );
 }
